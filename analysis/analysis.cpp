8894a4dcfd8cec843156f2e6089281dff2bcfd73
ZhangWei <suvtab@gmail.com>
2017-10-29 15:23:12 +0800

    Follow LLVM coding style

--- analysis/analysis.cpp
+++ analysis/analysis.cpp
@@ -11,61 +11,61 @@
 using namespace llvm;
 
 class Analysis : public FunctionPass {
-   public:
-    Analysis() : FunctionPass(ID), sleepable({"__might_sleep"}) {
-        const std::string filename("allfunctions.txt");
+public:
+  Analysis() : FunctionPass(ID), sleepable({"__might_sleep"}) {
+    const std::string filename("allfunctions.txt");
 
-        std::ifstream ifs(filename);
+    std::ifstream ifs(filename);
 
-        std::string caller;
-        std::string callee;
+    std::string caller;
+    std::string callee;
 
-        while (!ifs.eof()) {
-            std::string function;
-            ifs >> function;
-            if (function.front() == '*') {
-                caller = function.substr(1);
-            } else if (function.front() == '>') {
-                callee = function.substr(1);
+    while (!ifs.eof()) {
+      std::string function;
+      ifs >> function;
+      if (function.front() == '*') {
+        caller = function.substr(1);
+      } else if (function.front() == '>') {
+        callee = function.substr(1);
 
-                caller2callee[caller].insert(callee);
-                callee2caller[callee].insert(caller);
-            }
-        }
-
-        ifs.close();
-
-        std::unordered_set<std::string>::size_type size = 0;
-        while (sleepable.size() > size) {
-            size = sleepable.size();
-
-            std::unordered_set<std::string> copy(sleepable);
-
-            for (auto& function : copy) {
-                sleepable.insert(callee2caller[function].cbegin(),
-                                 callee2caller[function].cend());
-            }
-        }
+        caller2callee[caller].insert(callee);
+        callee2caller[callee].insert(caller);
+      }
     }
 
-    virtual bool runOnFunction(Function& F) override {
-        StringRef name = F.getName();
-        if (sleepable.find(name.str()) == sleepable.end()) {
-            return false;
-        }
-        errs() << name << "\n";
-        return false;
+    ifs.close();
+
+    std::unordered_set<std::string>::size_type size = 0;
+    while (sleepable.size() > size) {
+      size = sleepable.size();
+
+      std::unordered_set<std::string> copy(sleepable);
+
+      for (auto &function : copy) {
+        sleepable.insert(callee2caller[function].cbegin(),
+                         callee2caller[function].cend());
+      }
     }
+  }
 
-    static char ID;
+  virtual bool runOnFunction(Function &F) override {
+    StringRef name = F.getName();
+    if (sleepable.find(name.str()) == sleepable.end()) {
+      return false;
+    }
+    errs() << name << "\n";
+    return false;
+  }
 
-   private:
-    using set = std::unordered_set<std::string>;
-    using map = std::unordered_map<std::string, set>;
+  static char ID;
 
-    map caller2callee;
-    map callee2caller;
-    set sleepable;
+private:
+  using set = std::unordered_set<std::string>;
+  using map = std::unordered_map<std::string, set>;
+
+  map caller2callee;
+  map callee2caller;
+  set sleepable;
 };
 
 char Analysis::ID = 0;
